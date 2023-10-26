e1fe0e19ec49fea7300751296ba54023992ca749
divyaaddala <145589942+divyaaddala@users.noreply.github.com>
2023-10-26 09:29:32 -0700

    Update and rename login.html to login.php

---
+++ login.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="authenticate.php" method="post">
+  Email: <input type="text" name="email"><br>
+  Password: <input type="password" name="password"><br>
+  <input type="submit" value="Submit">
+</form>
+
+</body>
+</html>
