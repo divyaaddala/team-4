7fe5067f0d0e74bc99540d563dab9cc08e31f878
divyaaddala <145589942+divyaaddala@users.noreply.github.com>
2023-10-26 09:57:17 -0700

    Update login.php

--- login.php
+++ login.php
@@ -30,3 +30,24 @@ if ($email === $valid_email && $password === $valid_password) {
     echo 'Invalid email or password. Please try again.';
 }
 ?>
+<?php
+session_start();
+
+// Check if the user is authenticated
+if (!isset($_SESSION['authenticated']) || $_SESSION['authenticated'] !== true) {
+    // Redirect back to the login page if not authenticated
+    header('Location: login.php');
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Welcome, Divya! You can edit your website here.</h2>
+
+<!-- Your website editing content goes here -->
+
+</body>
+</html>
