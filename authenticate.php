26d9d75fdd0d0c07911c6ad8c328e06fbf6bf606
Apoorvabathula <145590500+Apoorvabathula@users.noreply.github.com>
2023-11-26 11:17:07 -0800

    Update authenticate.php

--- authenticate.php
+++ authenticate.php
@@ -1,19 +1,46 @@
 <?php
-$valid_email = 'divya.addala07@gmail.com';
-$valid_password = 'your_password'; // Replace with your actual password
+// Replace these with your actual credentials
+$valid_username = 'user123';
+$valid_password = 'password123';
 
-$email = $_POST['email'];
-$password = $_POST['password'];
+// Check if the form is submitted
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Retrieve user input
+    $entered_username = $_POST['username'];
+    $entered_password = $_POST['password'];
 
-if ($email === $valid_email && $password === $valid_password) {
-    // Set a session variable to mark the user as authenticated
-    session_start();
-    $_SESSION['authenticated'] = true;
-
-    // Redirect to your website editing page
-    header('Location: edit.php');
-    exit();
-} else {
-    echo 'Invalid email or password. Please try again.';
+    // Validate the credentials
+    if ($entered_username === $valid_username && $entered_password === $valid_password) {
+        // Authentication successful
+        // Redirect to the dashboard or any other page
+        header('Location: dashboard.html');
+        exit();
+    } else {
+        // Authentication failed
+        echo 'Invalid username or password. Please try again.';
+    }
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+</head>
+<body>
+
+<h2>Login</h2>
+<form action="" method="post">
+    <label for="username">Username:</label>
+    <input type="text" id="username" name="username" required>
+
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password" required>
+
+    <button type="submit">Login</button>
+</form>
+
+</body>
+</html>
